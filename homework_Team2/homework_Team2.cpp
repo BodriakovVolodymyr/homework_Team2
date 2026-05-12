db170cdc09a68047b588b460cf9aa9c6574618e0
Dikel <dikel23149@gmail.com>
2026-05-12 21:40:15 +0300

    Add project files.

---
+++ homework_Team2/homework_Team2.cpp
@@ -0,0 +1,12 @@
+﻿// homework_Team2.cpp : Defines the entry point for the application.
+//
+
+#include "homework_Team2.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ homework_Team2/homework_Team2.h
@@ -0,0 +1,8 @@
+﻿// homework_Team2.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
